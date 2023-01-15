fc8010b2cc237ac480afb35c8d4171e8191082c6
kashi <kashinathjeure@gmail.com>
2023-01-15 22:54:34 +0530

    String
    String

---
+++ 04_string.js
@@ -0,0 +1,10 @@
+var totalString = function(str){
+  
+    console.log('Total length', str.length);
+    console.log('char s index ', str.indexOf('S'));
+    console.log('index of lang ', str.indexOf('lang'));
+    console.log('last property', str.charAt(str.length-2));
+    console.log('last property', str.charAt(str.length-3));
+     
+}
+totalString('JavaScript the most popular language');
