d930e007b198b76a89e919af725251cd25c86211
kashi <kashinathjeure@gmail.com>
2023-01-15 22:53:42 +0530

    JS Basic
    JS Basic

---
+++ 01_basic.js
@@ -0,0 +1,24 @@
+console.log(20+20);
+console.log(20*20);
+console.warn('I am Angular Developer');
+console.info('I am Angular Developer')
+console.error("1+1,2+2")
+
+
+var str = 'Sachin';
+var num = 22;
+var isAva = true;
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof isAva);
+res = typeof str;
+console.log(res);
+
+var num_1 = Number;
+console.log(typeof num_1);
+
+var str_1 = String;
+console.log(typeof str_1);
+
+var isAva_1 = Boolean
+console.log(typeof isAva_1);
