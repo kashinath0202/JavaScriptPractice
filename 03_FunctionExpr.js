ab737647e7d8dabea1c845dc0f76c2c68c4ebb9f
kashi <kashinathjeure@gmail.com>
2023-01-15 22:54:26 +0530

    Function Experssion
    Function Experssion

---
+++ 03_FunctionExpr.js
@@ -0,0 +1,24 @@
+var squareNumber = function(num1){
+
+     console.log(num1*num1);
+}
+squareNumber(5);
+squareNumber(10);
+squareNumber(50);
+squareNumber(100);
+
+console.log(typeof squareNumber);
+
+var areaOfTriangle = function(num_1,num_2){
+
+      console.log('areaOfTriangle',(num_1*num_2)/2);
+}
+areaOfTriangle(45,34);
+
+
+var areaOfRectangle = function(hight,width){
+
+
+     console.log('areaOfRectangle',hight*width);
+}
+areaOfRectangle(33,33);
